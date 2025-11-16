3b7c05ab39faa5862c38cce9dcf7ae7ea7fb42ea
Harishanth03 <kandeepanharishanth@gmail.com>
2025-11-16 20:07:34 +0530

    feat: integrate Tailwind CSS and update App component layout
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Updated the App component to use Tailwind CSS for styling, enhancing the layout with responsive design.
    - Created an index.css file to import Tailwind CSS styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="flex md:flex-row flex-col gap-7 justify-center items-center w-full min-h-screen ">
+      <div className=" flex justify-center items-center w-full h-12 border-red-600 border-2">
+        <h1 className="md:text-3xl text-blue-600 font-semibold md:text-red-500">Hello World</h1>
+      </div>
+      <div className=" flex justify-center items-center w-full h-12 border-red-600 border-2">
+        <h1 className="md:text-3xl text-blue-600 font-semibold md:text-red-500">Hello World2</h1>
+      </div>
     </div>
   )
 }
